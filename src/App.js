2ffc93c4dcf6b351e83be8acb932d4a7f74d1c35
deeem <karpov.dimon@gmail.com>
2019-08-08 09:47:35 +0300

    ad desktop banner

--- src/App.js
+++ src/App.js
@@ -40,6 +40,23 @@ function App() {
           <img src={SignOutIcon} alt="sign out" className="header__sign-out" />
         </div>
       </section>
+
+      <section id="banner">
+        <div className="banner-left">
+          <h1 className="banner__title">Test assignment for Frontend Developer position</h1>
+          <p className="banner__text">
+            We kindly remind you that your test assignment should be submitted
+            as a link to github/bitbucket repository. Please be patient, we
+            consider and respond to every application that meets minimum
+            requirements. We look forward to your submission. Good luck!
+          </p>
+          <div className="banner__button">
+            <a href="#" className="btn btn-primary">Sign Up</a>
+          </div>
+
+        </div>
+        <div className="banner-right" />
+      </section>
     </div>
   )
 }
