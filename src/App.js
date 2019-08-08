49ed25279914949019578d6a05556b0c77e994db
deeem <karpov.dimon@gmail.com>
2019-08-08 18:05:23 +0300

    add requirements desktop

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import AboutImg from './assets/imgs/man-mobile.svg'
 import JSImage from './assets/imgs/javascript.svg'
 import CSSImage from './assets/imgs/css.svg'
 import HTMLImage from './assets/imgs/html.svg'
+import RequirementsImage from './assets/imgs/man-laptop-v1.svg'
 
 function App() {
   return (
@@ -118,6 +119,37 @@ function App() {
               typed, prototype-based and multi-paradigm.
             </p>
           </div>
+        </div>
+      </section>
+
+      <section id="requirements">
+        <h2 className="requirements__title">General requirements for the test task</h2>
+        <div className="requirements__wrapper">
+          <div className="requirements__text">
+            <p>
+              Users want to find answers to their questions quickly and data
+              shows that people really care about how quickly their pages load.
+              The Search team announced speed would be a ranking signal for
+              desktop searches in 2010 and as of this month (July 2018), page
+              speed will be a ranking factor for mobile searches too.  
+            </p>
+            <p>
+               If you're a developer working on a site, now is a good time to
+              evaluate your performance using our speed tools. Think about how
+              performance affects the user experience of your pages and consider
+              measuring a variety of real-world user-centric performance
+              metrics.
+            </p>
+            <p>
+              Are you shipping too much JavaScript? Too many images? Images and
+              JavaScript are the most significant contributors to the page
+              weight that affect page load time based on data from HTTP Archive
+              and the Chrome User Experience Report - our public dataset for key
+              UX metrics as experienced by Chrome users under real-world
+              conditions.
+            </p>
+          </div>
+          <img src={RequirementsImage} alt="requirements image"/>
         </div>
       </section>
     </div>
